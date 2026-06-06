0deffc8cf68a44e9a5c9f5ae9df0df359606285f
tuliothaua <tuliothauadutra@uniplaclages.edu.br>
2026-06-06 18:24:41 -0300

    feat: implement normalize-services and entities

---
+++ ProjetoFinalDevTulio.domain/Entities/Entity.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjetoFinalDevTulio.domain.Entities;
+
+// Classe base para todas as entidades, garantindo identidade única e validação de ID
+public abstract class Entity
+{
+    public int Id { get; protected set; }
+
+    protected Entity(int id = 0)
+    {
+        if (id < 0) throw new Exception("ID_NEGATIVO");
+
+        Id = id;
+    }
+}
