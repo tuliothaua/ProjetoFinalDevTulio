1f19ee9f7791d543eb2a53dede383a2f548bef60
tuliothaua <tuliothauadutra@uniplaclages.edu.br>
2026-06-06 19:38:43 -0300

    feat: implement interface in the project

---
+++ ProjetoFinalDevTulio.domain/Interfaces/ITarefaRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProjetoFinalDevTulio.domain.Entities;
+
+namespace ProjetoFinalDevTulio.domain.Interfaces;
+
+public interface ITarefaRepository
+{
+    Task<IEnumerable<TarefaItem>> ObterTodos();
+
+    Task<TarefaItem?> ObterPorId(int id);
+
+    Task<TarefaItem> Adicionar(TarefaItem tarefaItem);
+
+    Task<TarefaItem> Atualizar (TarefaItem tarefaItem);
+
+    Task<bool> Excluir(int id);
+}
