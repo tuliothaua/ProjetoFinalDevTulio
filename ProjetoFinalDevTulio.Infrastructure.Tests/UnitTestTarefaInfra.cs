3f632fe202929934ba34e1aa07ddad1050783da1
tuliothaua <tuliothauadutra@uniplaclages.edu.br>
2026-06-07 21:29:48 -0300

    feat: add infrastructure in the project using sqlite

---
+++ ProjetoFinalDevTulio.Infrastructure.Tests/UnitTestTarefaInfra.cs
@@ -0,0 +1,66 @@
+﻿using ProjetoFinalDevTulio.domain.Entities;
+using ProjetoFinalDevTulio.domain.Enums;
+using ProjetoFinalDevTulio.Infrastructure.Repositories;
+namespace ProjetoFinalDevTulio.Infrastructure.Tests;
+
+public class UnitTestTarefaInfra
+{
+    [Fact]
+    public async Task Tarefa_ObterTodos()
+    {
+        // ObterTodos
+        var repoTarefaTodos = new TarefaRepository();
+        var resultado = await repoTarefaTodos.ObterTodos();
+        Assert.NotNull(resultado);
+    }
+
+    [Fact]
+    public async Task Tarefa_Adicionar()
+    {
+        // cria objeto do tipo TarefaItem
+        ETarefaTipo tipos = ETarefaTipo.Casa | ETarefaTipo.Trabalho;
+        TarefaItem tarefa = TarefaItem.Criar(0, "Título da Tarefa2", "Descrição da Tarefa2", ETarefaStatus.Pendente, tipos, DateTime.Now, DateTime.Now.AddDays(7));
+        // cria repositório e adiciona a tarefa
+        var repoTarefaAdd = new TarefaRepository();
+        var TarefaInserido = await repoTarefaAdd.Adicionar(tarefa);
+        Assert.NotNull(TarefaInserido);
+        Assert.True(TarefaInserido.Id > 0);
+    }
+
+    [Fact]
+    public async Task Tarefa_ObterId_Atualizar()
+    {
+        // Criar e inserir uma tarefa para garantir que exista um registro para atualizar
+        ETarefaTipo tipos = ETarefaTipo.Casa;
+        var repoTarefaAdd = new TarefaRepository();
+        var novaTarefa = TarefaItem.Criar(0, "Título para atualizar", "Descrição inicial", ETarefaStatus.Pendente, tipos, DateTime.Now, DateTime.Now.AddDays(7));
+        var inserida = await repoTarefaAdd.Adicionar(novaTarefa);
+        Assert.NotNull(inserida);
+        // ObterPorId - existente para edição
+        var repoTarefaBuscaId = new TarefaRepository();
+        var TarefaPorId = await repoTarefaBuscaId.ObterPorId(inserida.Id);
+        Assert.NotNull(TarefaPorId);
+        // criar um novo objeto com os dados atualizados
+        var TarefaAtualizado = TarefaItem.Criar(TarefaPorId.Id, "Título atualizado", "Descrição atualizada", TarefaPorId.Status, TarefaPorId.Tipo,
+        TarefaPorId.DataCriacao, DateTime.Now.AddDays(20));
+        // atualizar a tarefa no repositório
+        var repoTarefaEdit = new TarefaRepository();
+        var resultadoAtualizacao = await repoTarefaEdit.Atualizar(TarefaAtualizado);
+        Assert.NotNull(resultadoAtualizacao);
+        Assert.Equal("TÍTULO ATUALIZADO", resultadoAtualizacao.Titulo);
+        Assert.Equal("Descrição atualizada", resultadoAtualizacao.Descricao);
+    }
+    [Fact]
+    public async Task Tarefa_Excluir_ObterId()
+    {
+        // Criar e inserir uma tarefa para garantir que exista um registro para remover
+        ETarefaTipo tipos = ETarefaTipo.Casa;
+        var repoTarefaAdd = new TarefaRepository();
+        var novaTarefa = TarefaItem.Criar(0, "Título para excluir", "Descrição remover", ETarefaStatus.Pendente, tipos, DateTime.Now, DateTime.Now.AddDays(7));
+        var inserida = await repoTarefaAdd.Adicionar(novaTarefa);
+        Assert.NotNull(inserida);
+        var repoTarefaExcluir = new TarefaRepository();
+        var resultadoExcluir = await repoTarefaExcluir.Excluir(inserida.Id);
+        Assert.True(resultadoExcluir);
+    }
+}
---
+++ ProjetoFinalDevTulio.Infrastructure/Data/DbProvider.cs
@@ -0,0 +1,52 @@
+﻿using System.Data;
+using System.Data.Common;
+using Microsoft.Data.Sqlite;
+namespace ProjetoFinalDevTulio.Infrastructure.Data;
+
+public static class DbProvider
+{
+    public static DbConnection CreateConnection()
+    {
+        try
+        {
+            // Registrar o provedor do SQLite no DbProviderFactories - Isso é obrigatório no .NET Core/5+ para que o GetFactory funcione.
+            DbProviderFactories.RegisterFactory("Microsoft.Data.Sqlite", SqliteFactory.Instance);
+            // Obter a fábrica (Factory) usando o nome do provedor registrado
+            DbProviderFactory factory = DbProviderFactories.GetFactory("Microsoft.Data.Sqlite");
+            // String de conexão para um banco em memória: "Data Source=:memory:"; // String de conexão para um banco em arquivo: "Data Source=banco.db"
+            string connectionString = "Data Source=db_tarefas.db";
+            DbConnection connection = new SqliteConnection(connectionString);
+            return connection ?? throw new Exception($"FALHA_CONEXAO {connectionString}");
+        }
+        catch (DbException ex) { throw new Exception($"FALHA_CONEXAO", ex); }
+    }
+    public static DbCommand CreateCommand(string commandText, DbConnection connection, CommandType commandType = CommandType.Text)
+    {
+        if (connection == null) { throw new ArgumentNullException($"COMMAND_CONEXAO_NULL"); }
+        if (string.IsNullOrWhiteSpace(commandText)) { throw new ArgumentException($"COMMAND_TEXT_VAZIO"); }
+        try
+        {
+            var command = connection.CreateCommand() ?? throw new Exception("FALHA_CRIAR_COMANDO");
+            command.CommandText = commandText;
+            command.CommandType = commandType;
+            command.CommandTimeout = 15;
+            return command;
+        }
+        catch (DbException ex) { throw new Exception($"FALHA_CRIAR_COMANDO", ex); }
+    }
+    public static DbParameter CreateParameter(string name, object value, DbType dbType)
+    {
+        if (string.IsNullOrWhiteSpace(name)) { throw new ArgumentException($"PARAMETER_VAZIO"); }
+        try
+        {
+            DbParameter parameter = new SqliteParameter
+            {
+                ParameterName = name,
+                Value = value ?? DBNull.Value,
+                DbType = dbType
+            };
+            return parameter;
+        }
+        catch (DbException ex) { throw new Exception($"ERRO_CRIAR_PARAMETRO", ex); }
+    }
+}
